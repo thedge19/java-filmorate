53b995447ca5a7f25b3f6cc6dab9b2b4d6baf759
thedge19 <paz80@yandex.ru>
2024-12-01 22:05:11 +0300

    fix/Исправление замечаний ревьера#2-2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate;
 
 import jakarta.validation.Validation;
-import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
