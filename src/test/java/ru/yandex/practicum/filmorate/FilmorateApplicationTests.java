22ab39cb47238b6fd46bcfc93c92ebdc10ef03a8
thedge19 <paz80@yandex.ru>
2024-12-21 18:58:54 +0300

    fix/корректировка после чекстайла гх #2

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -34,12 +34,45 @@ class FilmorateApplicationTests {
         userController = new UserController(new UserService(new InMemoryUserStorage()));
         filmsToAdd = new ArrayList<>();
         usersToAdd = new ArrayList<>();
-        filmController.create(new Film());
-        filmController.create(new Film());
-        filmController.create(new Film());
-        userController.create(new User());
-        userController.create(new User());
-        userController.create(new User());
+
+        Film film1 = new Film();
+        film1.setName("film1");
+        film1.setDescription("film1");
+        film1.setReleaseDate("2020-10-01");
+        film1.setDuration(120);
+        Film film2 = new Film();
+        film2.setName("film2");
+        film2.setDescription("film2");
+        film2.setReleaseDate("2020-10-02");
+        film2.setDuration(120);
+        Film film3 = new Film();
+        film3.setName("film3");
+        film3.setDescription("film3");
+        film3.setReleaseDate("2020-10-03");
+        film3.setDuration(120);
+
+        User user1 = new User();
+        user1.setLogin("u1");
+        user1.setName("user1");
+        user1.setEmail("user1@email.com");
+        user1.setBirthday("2000-01-01");
+        User user2 = new User();
+        user2.setLogin("u2");
+        user2.setName("user2");
+        user2.setEmail("user2@email.com");
+        user2.setBirthday("2000-02-02");
+        User user3 = new User();
+        user3.setLogin("u3");
+        user3.setName("user3");
+        user3.setEmail("user3@email.com");
+        user3.setBirthday("2000-03-03");
+
+        filmController.create(film1);
+        filmController.create(film2);
+        filmController.create(film3);
+        userController.create(user1);
+        userController.create(user2);
+        userController.create(user3);
     }
 
     @Test
