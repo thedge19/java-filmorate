3cf16da6f96e136b6642bdda086ae37db657a443
thedge19 <paz80@yandex.ru>
2025-01-07 02:27:32 +0300

    feat/добавлены db Storage#3

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -32,6 +32,6 @@ public class Film {
     private Integer duration;
     private Set<Long> likedUsersIds = new HashSet<>();
 
-    private Mpa Mpa;
+    private Mpa mpa;
     private List<Long> genreIds = new ArrayList<>();
 }
